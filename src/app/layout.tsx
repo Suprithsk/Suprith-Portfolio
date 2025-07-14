336120097e512957636e14366a7c5653f12c29de
Suprith SK <139979177+suprithsk2001@users.noreply.github.com>
2025-07-14 16:33:58 +0530

    fix: added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/next"
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
@@ -65,6 +65,7 @@ export default function RootLayout({
           <TooltipProvider delayDuration={0}>
             {children}
             <Navbar />
+            <Analytics />
           </TooltipProvider>
         </ThemeProvider>
       </body>
